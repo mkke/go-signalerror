d395746022f7977ccd10bbc10c594f83f2b9cf3b
Michael Stürmer <ms@mallorn.de>
2020-11-09 09:36:22 +0100

    initial import

---
+++ signalerror.go
@@ -0,0 +1,38 @@
+package signalerror
+
+import (
+	"os"
+	"syscall"
+)
+
+// NewSignalError returns a new error wrapping a signal
+func NewSignalError(signal os.Signal) *ErrSignal {
+	return &ErrSignal{signal}
+}
+
+// ErrSignal is returned as error when a signal was caught
+type ErrSignal struct {
+	Signal os.Signal
+}
+
+func (e *ErrSignal) Error() string {
+	return "caught signal " + e.Signal.String()
+}
+
+// ExitCode returns the best-current-practice exit code for a signal
+func (e *ErrSignal) ExitCode() int {
+	origSignal, ok := e.Signal.(syscall.Signal)
+	if !ok {
+		origSignal = 0
+	}
+	return 128 + int(origSignal)
+}
+
+// ErrSignalExitCode returns the signal's best-current-practice exit code, if err was a ErrSignal
+func ErrSignalExitCode(err error) (int, bool) {
+	if err, ok := err.(*ErrSignal); ok {
+		return err.ExitCode(), true
+	} else {
+		return 0, false
+	}
+}
